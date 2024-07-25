78c58d451358e3102db2af667247ac49952b275e
xyzxyz949 <176542672+xyzxyz949@users.noreply.github.com>
2024-07-24 20:27:08 -0500

    Update metadata.py

--- facefusion/metadata.py
+++ facefusion/metadata.py
@@ -2,7 +2,7 @@ METADATA =\
 {
 	'name': 'FaceFusion',
 	'description': 'Next generation face swapper and enhancer',
-	'version': '2.6.1',
+	'version': '2.6.2',
 	'license': 'MIT',
 	'author': 'Henry Ruhs',
 	'url': 'https://facefusion.io'
